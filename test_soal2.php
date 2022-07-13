8d4f9054722f6390429104951e9d779f725365fa
RezaWidyanto <35283323+RezaWidyanto@users.noreply.github.com>
2022-07-13 09:34:02 +0700

    Update test_soal2.php

--- test_soal2.php
+++ test_soal2.php
@@ -1,19 +1,21 @@
 <?php
 
 $data = [1, "ka", 67, "wah", "1772", "edukasi", 7, 98, -1];
-for ($d = 0; $d <= 8; $d++) {
-    if (is_int($data[$d]) && $data[$d] > 0) {
-        echo "index ke-". $d . " adalah integer dengan data "
-        . $data[$d] . "<br>";
+
+for ($hasil = 0; $hasil <= 8; $hasil++) {
+
+    if (is_int($data[$hasil]) && $data[$hasil] > 0) {
+        echo "index ke-". $hasil . " adalah integer dengan data "
+        . $data[$hasil] . "<br>";
     }
     
-    if (is_string($data[$d])) {
-        echo "index ke-". $d . " adalah string dengan data "
-        . $data[$d] . "<br>";
+    if (is_string($data[$hasil])) {
+        echo "index ke-". $hasil . " adalah string dengan data "
+        . $data[$hasil] . "<br>";
     }
     
-    if ($data[$d] < 0) {
-        echo "index ke-". $d . " tidak bisa divalidasi";
+    if ($data[$hasil] < 0) {
+        echo "index ke-". $hasil . " tidak bisa divalidasi";
     }
     
 }
