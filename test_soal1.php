102728007c39122e37cb28e06bd703015b715cb8
RezaWidyanto <rezawidyanto12x@gmail.com>
2022-07-13 09:26:35 +0700

    rezawidyanto : Test Kawah Edukasi bath III

---
+++ test_soal1.php
@@ -0,0 +1,21 @@
+<?php
+for ($i = 1; $i <= 200; $i++)
+{
+  if ( $i%5 == 0 && $i%11 == 0 )
+   {
+     echo $i . " FizzBuzz"."\n" ;
+   }
+  else if ( $i%5 == 0 ) 
+   {
+     echo $i. " Fizz"."\n";
+   }
+     else if ( $i%11 == 0 ) 
+   {
+     echo $i. " Buzz"."\n";
+   }
+     else
+   {
+     echo $i."\n";
+   }
+ }
+?>
