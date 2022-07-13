268baea190d55da6298daad366587df7befa80e3
RezaWidyanto <35283323+RezaWidyanto@users.noreply.github.com>
2022-07-13 09:31:36 +0700

    Update test_soal1.php

--- test_soal1.php
+++ test_soal1.php
@@ -3,15 +3,15 @@ for ($i = 1; $i <= 200; $i++)
 {
   if ( $i%5 == 0 && $i%11 == 0 )
    {
-     echo $i . " FizzBuzz"."\n" ;
+     echo " FizzBuzz\n" ;
    }
   else if ( $i%5 == 0 ) 
    {
-     echo $i. " Fizz"."\n";
+     echo " Fizz\n";
    }
      else if ( $i%11 == 0 ) 
    {
-     echo $i. " Buzz"."\n";
+     echo " Buzz\n";
    }
      else
    {
